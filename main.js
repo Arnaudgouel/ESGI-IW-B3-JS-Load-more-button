f6c197c596ffda7b438a7e89d8ad33fea6cdf0bc
Arnaud Gouel <gouel.arnaud@gmail.com>
2023-05-02 16:56:11 +0200

    generate list concerts with button

--- main.js
+++ main.js
@@ -1,13 +1,92 @@
 const concerts = [
-  ['2000/01/01', 'Paris', 'Abba', 45],
-  ['2001/02/01', 'Lyon', 'Patoche', 30],
-  ['2004/01/01', 'Paris', 'The Weekend', 78],
-  ['2005/01/01', 'Toulouse', 'The Week', 12],
-  ['2006/01/01', 'Paris', 'Adèle', 49],
-  ['2007/05/01', 'Dijon', 'Angèle', 56],
-  ['2008/01/01', 'Paris', 'Lady Gaga', 79],
-  ['2009/01/01', 'Marseille', 'Roosevelt', 25],
-  ['2010/08/01', 'Rouen', 'VHS collection', 26],
-  ['2011/09/01', 'Paris', 'Beyoncé', 92],
-]
+  {
+    date: '2000/01/01', 
+    lieu: 'Paris', 
+    chanteur: 'Abba', 
+    prix: 45,
+  },
+  {
+    date: '2001/02/01', 
+    lieu: 'Lyon', 
+    chanteur: 'Patoche', 
+    prix: 30,
+  },
+  {
+    date: '2004/01/01', 
+    lieu: 'Paris', 
+    chanteur: 'The Weekend', 
+    prix: 78,
+  },
+  {
+    date: '2005/01/01', 
+    lieu: 'Toulouse', 
+    chanteur: 'The Week', 
+    prix: 12,
+  },
+  {
+    date: '2006/01/01', 
+    lieu: 'Paris', 
+    chanteur: 'Adèle', 
+    prix: 49,
+  },
+  {
+    date: '2007/05/01', 
+    lieu: 'Dijon', 
+    chanteur: 'Angèle', 
+    prix: 56,
+  },
+  {
+    date: '2008/01/01', 
+    lieu: 'Paris', 
+    chanteur: 'Lady Gaga', 
+    prix: 79,
+  },
+  {
+    date: '2009/01/01', 
+    lieu: 'Marseille', 
+    chanteur: 'Roosevelt', 
+    prix: 25,
+  },
+  {
+    date: '2010/08/01', 
+    lieu: 'Rouen', 
+    chanteur: 'VHS collection', 
+    prix: 26,
+  },
+  {
+    date: '2011/09/01', 
+    lieu: 'Paris', 
+    chanteur: 'Beyoncé', 
+    prix: 92,
+  },
+];
+const step = 3;
+let indice = 0;
+let button = document.querySelector('button');
 
+const list = document.querySelector('#content');
+
+function createItem(item)
+{
+  let htmlItem = document.createElement('li');
+  htmlItem.innerHTML = `
+    <p>${item.chanteur}</p>
+    <p>à ${item.lieu} le ${item.date}</p>
+    <span>${item.prix}€</span>
+  `;
+
+  list.appendChild(htmlItem);
+}
+
+function generateItems()
+{
+  const max = indice+step < concerts.length ? indice+step : concerts.length
+  for(indice; indice < max; indice++){
+    createItem(concerts[indice]);
+  }
+  if (indice === concerts.length){
+    button.classList.add('hide');
+  }
+}
+
+button.addEventListener('click', generateItems)
