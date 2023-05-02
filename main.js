2f5d6935d4bbe97f396e9a4c09d55d0f2eea813e
Arnaud Gouel <gouel.arnaud@gmail.com>
2023-05-02 16:17:50 +0200

    init commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const concerts = [
+  ['2000/01/01', 'Paris', 'Abba', 45],
+  ['2001/02/01', 'Lyon', 'Patoche', 30],
+  ['2004/01/01', 'Paris', 'The Weekend', 78],
+  ['2005/01/01', 'Toulouse', 'The Week', 12],
+  ['2006/01/01', 'Paris', 'Adèle', 49],
+  ['2007/05/01', 'Dijon', 'Angèle', 56],
+  ['2008/01/01', 'Paris', 'Lady Gaga', 79],
+  ['2009/01/01', 'Marseille', 'Roosevelt', 25],
+  ['2010/08/01', 'Rouen', 'VHS collection', 26],
+  ['2011/09/01', 'Paris', 'Beyoncé', 92],
+]
+
